744c0ced15fbe04a224fbdd494abc0b0618dc57b
BruhaBruh <drugsho.jaker@gmail.com>
2022-03-26 10:18:07 +0500

    try catch in fetchEmotes, TODO add erorr log

--- src/lib/fetchEmotes.ts
+++ src/lib/fetchEmotes.ts
@@ -11,18 +11,20 @@ const fetchAllEmotes = async (channel: string, broadcasterId: string) => {
 	})
 		.then((res) => res.json())
 		.catch(console.error);
-
-	const stvChatEmotes: STVEmote[] = await fetch(
-		'https://api.7tv.app/v2/users/' + channel + '/emotes',
-		{
-			method: 'GET'
-		}
-	)
-		.then((res) => res.json())
-		.catch(console.error);
-
 	stvemotes.add(stvGlobalEmotes);
-	stvemotes.add(stvChatEmotes);
+
+	try {
+		const stvChatEmotes: STVEmote[] = await fetch(
+			'https://api.7tv.app/v2/users/' + channel + '/emotes',
+			{
+				method: 'GET'
+			}
+		)
+			.then((res) => res.json())
+			.catch(console.error);
+
+		stvemotes.add(stvChatEmotes);
+	} catch (e) {}
 
 	const bttvGlobalEmotes: BTTVEmote[] = await fetch(
 		'https://api.betterttv.net/3/cached/emotes/global',
@@ -32,36 +34,40 @@ const fetchAllEmotes = async (channel: string, broadcasterId: string) => {
 	)
 		.then((res) => res.json())
 		.catch(console.error);
-
-	const bttvChatEmotes: {
-		id: string;
-		bots: any[];
-		avatar: string;
-		channelEmotes: BTTVEmote[];
-		sharedEmotes: BTTVEmote[];
-	} = await fetch('https://api.betterttv.net/3/cached/users/twitch/' + broadcasterId, {
-		method: 'GET'
-	})
-		.then((res) => res.json())
-		.catch(console.error);
-
 	bttvemotes.add(bttvGlobalEmotes);
-	bttvemotes.add(bttvChatEmotes.sharedEmotes);
-	bttvemotes.add(bttvChatEmotes.channelEmotes);
 
-	const ffzChatEmotes: {
-		sets: {
-			[key: string]: {
-				emoticons: FFZEmote[];
+	try {
+		const bttvChatEmotes: {
+			id: string;
+			bots: any[];
+			avatar: string;
+			channelEmotes: BTTVEmote[];
+			sharedEmotes: BTTVEmote[];
+		} = await fetch('https://api.betterttv.net/3/cached/users/twitch/' + broadcasterId, {
+			method: 'GET'
+		})
+			.then((res) => res.json())
+			.catch(console.error);
+
+		bttvemotes.add(bttvChatEmotes.sharedEmotes);
+		bttvemotes.add(bttvChatEmotes.channelEmotes);
+	} catch (e) {}
+
+	try {
+		const ffzChatEmotes: {
+			sets: {
+				[key: string]: {
+					emoticons: FFZEmote[];
+				};
 			};
-		};
-	} = await fetch('https://api.frankerfacez.com/v1/room/id/' + broadcasterId, {
-		method: 'GET'
-	})
-		.then((res) => res.json())
-		.catch(console.error);
+		} = await fetch('https://api.frankerfacez.com/v1/room/id/' + broadcasterId, {
+			method: 'GET'
+		})
+			.then((res) => res.json())
+			.catch(console.error);
 
-	ffzemotes.add(ffzChatEmotes.sets[Object.keys(ffzChatEmotes.sets)[0]].emoticons);
+		ffzemotes.add(ffzChatEmotes.sets[Object.keys(ffzChatEmotes.sets)[0]].emoticons);
+	} catch (e) {}
 };
 
 export default fetchAllEmotes;
