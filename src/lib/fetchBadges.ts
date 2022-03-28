6ef3ddce9d5bfbf76324c647d6450f4814d3f34f
BruhaBruh <drugsho.jaker@gmail.com>
2022-03-29 01:49:47 +0500

    fix bug

--- src/lib/fetchBadges.ts
+++ src/lib/fetchBadges.ts
@@ -25,14 +25,11 @@ const fetchAllBadges = async (headers: Headers, broadcasterId: string): Promise<
 
 	globalBadges.data.forEach((twitchBadge) => {
 		const sameBadge = mergedBadges.find((m) => m.set_id === twitchBadge.set_id);
-		if (!sameBadge) return mergedBadges.push(sameBadge);
+		if (!sameBadge) return mergedBadges.push(twitchBadge);
 		const resultVersions = [
 			...sameBadge.versions,
 			...twitchBadge.versions.filter((tb) => !sameBadge.versions.map((v) => v.id).includes(tb.id))
 		];
-		console.log('Same: ', sameBadge);
-		console.log('Twitch: ', twitchBadge);
-		console.log('Result: ', { set_id: sameBadge.set_id, versions: resultVersions });
 		mergedBadges = [
 			...mergedBadges.filter((b) => b.set_id !== sameBadge.set_id),
 			{ set_id: sameBadge.set_id, versions: resultVersions }
